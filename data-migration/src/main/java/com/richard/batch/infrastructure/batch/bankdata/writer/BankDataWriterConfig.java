fa4cfbe2d3c06e36e82cc52102f1a8428be7618c
Ricardo Viana <richardluizv@gmail.com>
2022-05-26 05:21:08 -0300

    configurando escritores  de arquivos delimitado

--- data-migration/src/main/java/com/richard/batch/infrastructure/batch/bankdata/writer/BankDataWriterConfig.java
+++ data-migration/src/main/java/com/richard/batch/infrastructure/batch/bankdata/writer/BankDataWriterConfig.java
@@ -1,9 +1,24 @@
 package com.richard.batch.infrastructure.batch.bankdata.writer;
 
-import lombok.RequiredArgsConstructor;
+import com.richard.batch.domain.BankData;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@RequiredArgsConstructor
+import javax.sql.DataSource;
+
 @Configuration
 public class BankDataWriterConfig {
+
+    @Bean
+    public JdbcBatchItemWriter<BankData> personWriter(@Qualifier("appdatabase") DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<BankData>()
+                .dataSource(dataSource)
+                .sql("INSERT INTO dados_bancarios (id, pessoa_id, agencia, conta, banco) VALUES (:id, :personId, :agency, :account, :bank)")
+                .beanMapped()
+                .build();
+    }
+    
 }
