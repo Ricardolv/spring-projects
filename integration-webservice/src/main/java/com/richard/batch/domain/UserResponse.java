de367c255d728f891c7308931a524b9998bdf04b
Ricardo Viana <richardluizv@gmail.com>
2022-05-30 10:59:59 -0300

    implementando leituras paginasdas

--- integration-webservice/src/main/java/com/richard/batch/domain/UserResponse.java
+++ integration-webservice/src/main/java/com/richard/batch/domain/UserResponse.java
@@ -1,18 +1,12 @@
 package com.richard.batch.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.util.List;
 
 @NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@Builder
 @Setter
 @Getter
 public class UserResponse {
--- integration-webservice/src/main/java/com/richard/batch/infrastructure/batch/reader/UserReader.java
+++ integration-webservice/src/main/java/com/richard/batch/infrastructure/batch/reader/UserReader.java
@@ -3,14 +3,19 @@ package com.richard.batch.infrastructure.batch.reader;
 import com.richard.batch.domain.User;
 import com.richard.batch.domain.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.annotation.AfterChunk;
+import org.springframework.batch.core.annotation.BeforeChunk;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,15 +24,52 @@ public class UserReader implements ItemReader<User> {
 
     private final RestTemplate restTemplate = new RestTemplate();
     private int page = 1;
+    private int userIndex = 0;
+    private int total = 0;
+    private List<User> users = new ArrayList<>();
+
+    @Value("${chunkSize}")
+    private int chunkSize;
+
+    @Value("${pageSize}")
+    private int pageSize;
+
+    @Value("${limit}")
+    private int limit;
 
     @Override
     public User read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-        return null;
+        User user;
+
+        if (userIndex < users.size())
+            user = users.get(userIndex);
+        else
+            user = null;
+
+        userIndex++;
+        return user;
     }
 
     private List<User> fetchUserDataFromApi() {
-        ResponseEntity<UserResponse> response = restTemplate.getForEntity(String.format("https://gorest.co.in/public/v2/users?page=%d", page), UserResponse.class);
+        ResponseEntity<UserResponse> response = restTemplate.getForEntity(String.format("https://gorest.co.in/public/v1/users?page=%d", page), UserResponse.class);
         return response.getBody().getData();
     }
 
+    @BeforeChunk
+    private void beforeChunk(ChunkContext chunkContext) {
+        System.out.println("Inicio do chunk");
+        for (int i = 0; i < chunkSize; i+= pageSize) {
+            if (total >= limit) return;
+            users.addAll(fetchUserDataFromApi());
+            total += users.size();
+            page++;
+        }
+    }
+
+    @AfterChunk
+    private void afterChunk(ChunkContext chunkContext) {
+        System.out.println("Fim do chunk");
+        userIndex = 0;
+        users = new ArrayList<>();
+    }
 }
